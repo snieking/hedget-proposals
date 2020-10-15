318f7bc851960326c74f4526a9a03b749e638e7a
Viktor Plane <viktorplane.sonie@gmail.com>
2020-10-15 06:16:52 +0000

    Code formatting

--- src/core/header/Header.tsx
+++ src/core/header/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Grid, makeStyles} from '@material-ui/core';
+import { Grid, makeStyles } from '@material-ui/core';
 import NavigationIcons from './NavigationIcons';
 import Logo from './Logo';
 
--- src/core/header/Logo.tsx
+++ src/core/header/Logo.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {makeStyles, Theme} from '@material-ui/core';
+import { makeStyles, Theme } from '@material-ui/core';
 import { Link as RouterLink } from 'react-router-dom';
 
 const useStyles = makeStyles((theme: Theme) => ({
